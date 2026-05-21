626912a60d41ecd366c46eed1d9618c922884241
Iwan Bagus S <stywn93@gmail.com>
2026-05-22 03:55:06 +0700

    input data wali done

--- src/automation.py
+++ src/automation.py
@@ -58,6 +58,38 @@ def main():
             btn_recheck.click()
             # Tunggu popup benar-benar hilang
             btn_recheck.wait_for(state="hidden", timeout=5000)
+            checkbox = page.locator("input[name='noNik']")
+            checkbox.set_checked(True, force=True)
+
+            page.locator("input#nik\\ wali").fill("3512145806880002")
+            page.locator('input[name="Nama Lengkap Wali"]').fill("ANNISATUL QORIAH")
+            # page.pause()
+
+            # page.locator("#Tanggal\\ Lahir .mx-input-wrapper").click()
+            # page.locator('[id="Tanggal Lahir"] .mx-input-wrapper').click()
+            page.locator('[id="Tanggal Lahir"] .mx-input-wrapper').filter(has_text="Pilih Tanggal Lahir").click()
+            # page.locator("i.mx-icon-calendar").click()
+            # page.pause()
+            popup = page.locator(".mx-datepicker-popup")
+            year_btn = popup.locator(".mx-btn-current-year")
+            prev_year = popup.locator(".mx-btn-icon-double-left")
+
+            while year_btn.text_content().strip() != "2008":
+                prev_year.click()
+
+            popup.locator('td.cell[title="2008-05-01"]').click()
+
+            page.locator("div:has(> .text-gray-4:text('Pilih Jenis Kelamin'))").click()
+            page.pause()
+            # panel.get_by_role("button", name="25").click()
+            # page.get_by_text("Pilih jenis kelamin", exact=True).click()
+            page.locator(".max-h-\\[250px\\]").get_by_text("Perempuan", exact=True).click()
+            # page.get_by_text("Laki-laki", exact=True).click()
+
+            # page.locator('input#No\\ Whatsapp').fill("812345678")
+            page.locator("label").filter(has_text="No. Whatsapp Wali").locator('input[name="Nomor whatsapp"]').fill(
+            "81234567890")
+
         except PlaywrightTimeoutError:
             # Popup tidak muncul = NIK valid, lanjut ke step berikutnya
             pass
