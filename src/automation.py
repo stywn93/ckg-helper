723b2030851a21bce206e93084a43dab38f74548
Iwan Bagus S <stywn93@gmail.com>
2026-05-23 01:55:32 +0700

    success read from excel

--- src/automation.py
+++ src/automation.py
@@ -52,6 +52,25 @@ def select_date_from_picker(page, field_selector: str, date_value: str) -> None:
 
     popup.locator(f'td.cell[title="{date_value}"]').click()
 
+def select_date_from_picker2(trigger_locator, date_value: str) -> None:
+    year, month, day = date_value.split("-")
+    month_label = MONTH_LABELS[month]
+
+    trigger_locator.click()
+
+    popup = trigger_locator.page.locator(".mx-datepicker-popup")
+    month_btn = popup.locator(".mx-btn-current-month")
+    prev_month = popup.locator(".mx-btn-icon-left")
+    year_btn = popup.locator(".mx-btn-current-year")
+    prev_year = popup.locator(".mx-btn-icon-double-left")
+
+    while month_btn.text_content().strip() != month_label:
+        prev_month.click()
+
+    while year_btn.text_content().strip() != year:
+        prev_year.click()
+
+    popup.locator(f'td.cell[title="{date_value}"]').click()
 
 def main():
     data_rows = load_rows_from_excel("data.xlsx")
@@ -78,33 +97,17 @@ def main():
         nik_input.fill(str(data["nik"]))
         page.locator('input#Nama\\ Lengkap').fill(str(data["nama_lengkap"]))
 
-        # page.locator("#Tanggal\\ Lahir .mx-input-wrapper").click()
-        # popup = page.locator(".mx-datepicker-popup")
-        # year_btn = popup.locator(".mx-btn-current-year")
-        # month_btn = popup.locator(".mx-btn-current-month")
-        # prev_year = popup.locator(".mx-btn-icon-double-left")
-        # prev_month = popup.locator(".mx-btn-icon-left")
-        #
-        # while month_btn.text_content().strip() != "Okt":
-        #     prev_month.click()
-        #
-        # while year_btn.text_content().strip() != "2010":
-        #     prev_year.click()
-        #
-        #
-        # # page.pause()
-        #
-        # popup.locator('td.cell[title="2010-10-01"]').click()
-        select_date_from_picker(page, "#Tanggal\\ Lahir .mx-input-wrapper", "2010-10-01")
+        select_date_from_picker(page, "#Tanggal\\ Lahir .mx-input-wrapper", str(data["tgl_lahir"]))
 
         page.get_by_text("Pilih jenis kelamin", exact=True).click()
-        page.get_by_text("Laki-laki", exact=True).click()
+        page.get_by_text(str(data["gender"]), exact=True).click()
 
-        page.locator('input#No\\ Whatsapp').fill("812345678")
-        page.pause()
+        page.locator('input#No\\ Whatsapp').fill(str(data["no_whatsapp"]))
+        # page.pause()
 
         panel = page.locator("div:has(> .text-\\[20px\\].font-bold:text('Tanggal Pemeriksaan'))")
-        panel.get_by_role("button", name="25").click()
+        # panel.get_by_role("button", str(data["tgl_pemeriksaan"])).nth(1).click()
+        panel.get_by_role("button", name=str(data["tgl_pemeriksaan"])).nth(1).click()
 
         page.get_by_role("button", name="Selanjutnya").click()
         btn_recheck = page.locator("button:has-text('Periksa Kembali')").first
@@ -115,55 +118,52 @@ def main():
             btn_recheck.wait_for(state="hidden", timeout=5000)
             checkbox = page.locator("input[name='noNik']")
             checkbox.set_checked(True, force=True)
-            page.locator("input#nik\\ wali").fill("3512052205820001")
-            page.locator('input[name="Nama Lengkap Wali"]').fill("KUSYONO")
+            page.locator("input#nik\\ wali").fill(str(data["nik_wali"]))
+            page.locator('input[name="Nama Lengkap Wali"]').fill(str(data["nama_wali"]))
 
-            # page.locator('[id="Tanggal Lahir"] .mx-input-wrapper').filter(has_text="Pilih Tanggal Lahir").click()
-            # popup = page.locator(".mx-datepicker-popup")
-            # month_btn = popup.locator(".mx-btn-current-month")
-            # prev_month = popup.locator(".mx-btn-icon-left")
-            # year_btn = popup.locator(".mx-btn-current-year")
-            # prev_year = popup.locator(".mx-btn-icon-double-left")
-            # while month_btn.text_content().strip() != "Mei":
-            #     prev_month.click()
-            # while year_btn.text_content().strip() != "1982":
-            #     prev_year.click()
-            # popup.locator('td.cell[title="1982-05-22"]').click()
-            select_date_from_picker(page,'[id="Tanggal Lahir"] .mx-input-wrapper',"1982-05-22")
-            select_date_from_picker(page, "#Tanggal\\ Lahir .mx-input-wrapper", "2010-10-01")
+            page.locator('[id="Tanggal Lahir"] .mx-input-wrapper').filter(has_text="Pilih Tanggal Lahir").click()
+
+            select_date_from_picker2(page.locator('[id="Tanggal Lahir"] .mx-input-wrapper').filter(has_text="Pilih Tanggal Lahir"),str(data["tgl_lahir_wali"])
+            )
 
             page.locator("div:has(> .text-gray-4:text('Pilih Jenis Kelamin'))").click()
-            page.locator(".max-h-\\[250px\\]").get_by_text("Laki-laki", exact=True).click()
+            page.locator(".max-h-\\[250px\\]").get_by_text(str(data["gender_wali"]), exact=True).click()
             page.locator("label").filter(has_text="No. Whatsapp Wali").locator('input[name="Nomor whatsapp"]').fill(
-            "81234567890")
+            str(data["no_whatsapp_wali"]))
             page.get_by_role("button", name="Selanjutnya").click()
             page.locator("button:has-text('Lanjutkan')").click()
-            page.pause()
+            # page.pause()
 
         except PlaywrightTimeoutError:
             btn_success.click()
 
         page.get_by_text("Pilih status pernikahan", exact=True).click()
-        page.get_by_text("Belum Menikah", exact=True).click()
+        page.get_by_text(str(data["pernikahan"]), exact=True).click()
 
         page.get_by_text("Pilih pekerjaan", exact=True).click()
-        page.get_by_text("Pegawai Swasta ", exact=True).click()
+        page.get_by_text(str(data["pekerjaan"]), exact=True).click()
 
         page.get_by_text("Pilih alamat domisili", exact=True).click()
-        page.get_by_text("Jawa Timur ", exact=True).click()
-        page.get_by_text("Kab. Situbondo ", exact=True).click()
-        page.get_by_text("Kendit ", exact=True).click()
-        page.get_by_text("Balung ", exact=True).click()
+        page.get_by_text(str(data["prov"]), exact=True).click()
+        page.get_by_text(str(data["kab"]), exact=True).click()
+        page.get_by_text(str(data["kec"]), exact=True).click()
+        page.get_by_text(str(data["desa"]), exact=True).click()
 
-        page.locator("textarea#detail-domisili").fill("Jl. ABCD")
+        page.locator("textarea#detail-domisili").fill(str(data["domisili"]))
 
         page.get_by_role("button", name="Selanjutnya").click()
         page.wait_for_timeout(1500)
         page.get_by_role("button", name="Daftarkan Tanpa NIK").click()
         page.wait_for_timeout(1500)
-        page.get_by_role("button", name="Selanjutnya").click()
+        # page.get_by_role("button", name="Selanjutnya").click()
         page.wait_for_load_state("networkidle")
-        page.get_by_role("button", name="Tutup").click()
+        page.get_by_role("button", name="Bantu Isi Skrining Mandiri").click()
+        page.wait_for_timeout(1500)
+        page.wait_for_load_state("networkidle")
+        checkbox = page.locator("input[id='sameLocation']")
+        checkbox.set_checked(True, force=True)
+        page.wait_for_timeout(2000)
+        page.get_by_role("button", name="Simpan").click()
 
         page.pause()
 
