8108d8cbad89af7955747221ca1b1aaeee21f497
Iwan Bagus S <stywn93@gmail.com>
2026-07-20 20:01:17 +0700

    feat: implement no-click-delay context manager for date picker functionality

--- src/helpers/click_delay.py
+++ src/helpers/click_delay.py
@@ -1,13 +1,26 @@
+import contextlib
 import os
 from playwright.sync_api import Locator
 
 CLICK_DELAY_MS = int(os.getenv("CKG_CLICK_DELAY_MS", "1000"))
+_delay_enabled = True
 
 if CLICK_DELAY_MS > 0:
     _original_click = Locator.click
 
     def _patched_click(self, **kwargs):
         _original_click(self, **kwargs)
-        self.page.wait_for_timeout(CLICK_DELAY_MS)
+        if _delay_enabled:
+            self.page.wait_for_timeout(CLICK_DELAY_MS)
 
     Locator.click = _patched_click
+
+
+@contextlib.contextmanager
+def no_click_delay():
+    global _delay_enabled
+    _delay_enabled = False
+    try:
+        yield
+    finally:
+        _delay_enabled = True
