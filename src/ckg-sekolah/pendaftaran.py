516634b00661ac9f88b395b7b98a5f3eb9bfcff9
Iwan Bagus S <stywn93@gmail.com>
2026-08-29 20:32:58 +0700

    feat: enhance data entry for guardian registration and improve date selection logic

--- src/ckg-sekolah/pendaftaran.py
+++ src/ckg-sekolah/pendaftaran.py
@@ -131,6 +131,7 @@ def wait_for_first_visible(page, locators_dict, timeout=5000):
 #         btn_success.click()
 
 def handle_periksa_kembali(page, data: dict, date_picker: DatePicker) -> None:
+    # print("handle periksa kembali executed")
     page.locator('input#Nama\\ Lengkap').fill(format_cell_value(data["nama_lengkap"]))
 
     date_picker.select(
@@ -145,21 +146,40 @@ def handle_periksa_kembali(page, data: dict, date_picker: DatePicker) -> None:
     ).click()
     
     page.locator('input#No\\ Whatsapp').fill(format_cell_value(data["no_whatsapp"]))
+    # print("end of handle periksa kembali")
 
     # Select a specific day button by exact day number (avoids "1" matching "11", "12"...)
     # dob = datetime.strptime(format_cell_value(data['tgl_lahir']), "%Y-%m-%d")
 
 def isi_data_wali(page, data: dict, date_picker: DatePicker) -> None:
-    page.locator("input#nik\\ wali").fill(format_cell_value(data["nik_wali"]))
-    page.locator('input[name="Nama Lengkap Wali"]').fill(format_cell_value(data["nama_wali"]))
+    print("isi data wali executed")
+    page.get_by_role("textbox", name="NIK *").click()
+    page.get_by_role("textbox", name="NIK *").fill(format_cell_value(data["nik_wali"]))
+    page.locator("[id=\"Nama Lengkap-wali\"]").click()
+    page.locator("[id=\"Nama Lengkap-wali\"]").fill(format_cell_value(data["nama_wali"]))
+    # page.pause()
+    # page.locator("input#nik\\ wali").fill(format_cell_value(data["nik_wali"]))
+    # page.locator('input[name="Nama Lengkap Wali"]').fill(format_cell_value(data["nama_wali"]))
 
-    date_picker.select(page.locator('[id="Tanggal Lahir"] .mx-input-wrapper').filter(has_text="Pilih Tanggal Lahir"),format_cell_value(data["tgl_lahir_wali"]),)
+    # coba pakai ini
+    page.locator('[id="Tanggal Lahir-wali"]').locator(".mx-datepicker").click()
+
+    # combine single line dan library yang sudah ada
+    date_picker.select(
+        page.locator('[id="Tanggal Lahir-wali"]').locator(".mx-datepicker"),
+        format_cell_value(data["tgl_lahir_wali"]),
+    )
+
+    page.pause()
+    # print(page.locator("div.mb-2.font-semibold.text-sm", has_text="Jenis Kelamin").count())
+    # date_picker.select(page.locator('[id="Tanggal Lahir"] .mx-input-wrapper').filter(has_text="Pilih Tanggal Lahir"),format_cell_value(data["tgl_lahir_wali"]),)
 
     page.locator("div:has(> .text-gray-4:text('Pilih Jenis Kelamin'))").click()
     page.locator(".max-h-\\[250px\\]").get_by_text(format_cell_value(data["gender_wali"]), exact=True).click()
-    page.locator("label").filter(has_text="No. Whatsapp Wali").locator('input[name="Nomor whatsapp"]').fill(
-        format_cell_value(data["no_whatsapp_wali"])
-    )
+    # page.locator("label").filter(has_text="No. Whatsapp Wali").locator('input[name="Nomor whatsapp"]').fill(
+    #     format_cell_value(data["no_whatsapp_wali"])
+    # )
+    page.locator('[id="No whatsapp-wali"]').fill("81234567890")
 
 def register_single_entry(page, data: dict, row_number: int, date_picker: DatePicker) -> None:
     prepare_registration_page(page)
